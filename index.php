21312347b66c65f81104165902c3d6d5856fdc87
Reza Rezaei <merezarezaei@gmail.com>
2022-02-14 19:48:34 +0330

    start page of josi

---
+++ index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="fa">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>جسی نیازمندی انلاین</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Add icon library -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<style>
+    body {
+        background-color: black;
+        color: gold;
+    
+    }
+
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+
+    a :hover {
+        color: aqua
+    }
+
+    .zerobox{
+
+        border: 0px;
+        padding: 0px;
+        margin: 0px;
+    }
+</style>
+
+<body>
+    <div class="container .text-center">
+
+        <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 row">
+            <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12">
+                <img src="josi.jpg" alt="josi" style="width: 100%;">
+            </div>
+            <div class="col-xxl-9 col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12 .text-center" style="align-content: center;">
+
+                <h1 class=".text-center " style="text-align: center;">جسی نیازمندی های انلاین </h1>
+
+                <h2 class=".text-center " style="text-align: center;">جسی جایی است که شما می توانید نیازمندی های خود را رایگان ثبت کنید و توسط ما در شبکه های اجتماعی انتشار یابد</h2>
+            </div>
+        </div>
+
+        <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 row .text-center zerobox" style="align-content: center;">
+
+            <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12 row  zerobox">
+
+                <a class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 zerobox" href="https://t.me/josikaronlinechannel" style="text-decoration: none;align-self:center">
+                    <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 zerobox " style="text-align: center;">
+
+                        <h2 class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 zerobox">کانال تلگرام</h2>
+                    </div>
+
+                    <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 items-center zerobox" style="text-align: center;">
+
+
+                        <i class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 fa fa-telegram zerobox" style="text-decoration: none;font-size: 50px"></i>
+
+                    </div>
+                </a>
+
+            </div>
+
+            <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12 row  zerobox">
+
+                <a class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 zerobox" href="https://t.me/josikaronlinegroup" style="text-decoration: none;align-self:center">
+                    <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12  zerobox" style="text-align: center;">
+
+                        <h2 class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 zerobox">گروه تلگرام</h2>
+                    </div>
+
+                    <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 items-center zerobox" style="text-align: center;">
+
+
+                        <i class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 fa fa-telegram zerobox" style="text-decoration: none;font-size: 50px"></i>
+
+                    </div>
+                </a>
+
+
+            </div>
+
+            <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12 row zerobox ">
+
+                <a class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 zerobox" href="https://chat.whatsapp.com/HC9BTK8lsRzKHx21KkvRJs" style="text-decoration: none;align-self:center">
+                    <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 zerobox " style="text-align: center;">
+
+                        <h2 class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 zerobox"> 1گروه واتزاپ</h2>
+                    </div>
+
+                    <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 items-center zerobox" style="text-align: center;">
+
+                        <a class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 zerobox" href="https://chat.whatsapp.com/HC9BTK8lsRzKHx21KkvRJs" style="text-decoration: none;align-self:center">
+                            <i class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 fa fa-whatsapp zerobox" style="text-decoration: none !important;font-size: 50px"></i>
+
+                    </div>
+                </a>
+
+            </div>
+
+            <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12 row  zerobox">
+
+                <a class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 zerobox" href="https://chat.whatsapp.com/HC9BTK8lsRzKHx21KkvRJs" style="text-decoration: none;align-self:center">
+                    <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 zerobox " style="text-align: center;">
+
+                        <h2 class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 zerobox"> 2گروه واتزاپ</h2>
+                    </div>
+
+                    <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 items-center zerobox" style="text-align: center;">
+
+                        <a class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 zerobox" href="https://chat.whatsapp.com/GuIj3EWtZJzG8ObP4iRdQ1" style="text-decoration: none;align-self:center">
+                            <i class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 fa fa-whatsapp zerobox" style="text-decoration: none !important;font-size: 50px"></i>
+
+                    </div>
+                </a>
+
+            </div>
+
+
+            <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12 row  zerobox">
+
+                <a class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 zerobox" href="https://chat.whatsapp.com/HC9BTK8lsRzKHx21KkvRJs" style="text-decoration: none;align-self:center">
+                    <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 zerobox " style="text-align: center;">
+
+                        <h2 class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 zerobox"> 3گروه واتزاپ</h2>
+                    </div>
+
+                    <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 items-center zerobox" style="text-align: center;">
+
+                        <a class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 zerobox" href="https://chat.whatsapp.com/HjVt4dWJzJHK1bRvKYxp4a" style="text-decoration: none;align-self:center">
+                            <i class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 fa fa-whatsapp zerobox" style="text-decoration: none !important;font-size: 50px"></i>
+
+                    </div>
+                </a>
+
+            </div>
+
+
+        </div>
+
+        <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 row zerobox" style="text-align: center;">
+            <h1>برای ثبت اگهی به ادمین شبکه های اجتماعی پیام ارسال کنید</h1>
+        </div>
+
+        <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 row " style="text-align: center;">
+
+            <h2>شماره تماس</h2>
+            <a href="tel:+989228605165">
+
+            <h2>برای تماس اینجا ضربه بزنید</h2>
+            
+            
+            <h2>+98 922 86 05 165</h2>
+
+            </a>
+
+        </div>
+
+        <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 row zerobox" style="text-align: center; color:red">
+            <h1>این وب سایت در حال تکمیل میباشد و به زودی خدمات ثبت اگهی از طریق سایت در دسترس عموم قرار خواهد گرفت</h1>
+        </div>
+
+
+        <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 row zerobox" style="text-align: center;">
+            <h6>کلیه حقوق این سایت متعلق به شرکت فناوری اطلاعات شیراز اندیشه نوین میباشد</h6>
+        </div>
+    </div>
+
+
+
+    
+
+</body>
+
+</html>
